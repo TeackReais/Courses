ed68f19ef0992241333d415e4533f58f6cb865f0
TeackReais <teack_reais@outlook.com>
2020-12-18 17:04:10 +0800

    Update

--- Course11/proj4/main.cpp
+++ Course11/proj4/main.cpp
@@ -8,14 +8,18 @@ struct HomeEndKey
 {
     int start;
     int end;
-    char content[1000];
+    char content[100];
 };
 
 int main()
 {
-    char input[100000];
-    while (cin.getline(input, 100000)) //OJ EOF测试
+    char input[100];
+    while (cin.getline(input, 100)) //OJ EOF测试 cin.getline会清空变量原来的值
     {
+        if (input[0] == '\0')
+        {
+            break;
+        }
         solve(input);
     }
     return 0;
@@ -33,13 +37,22 @@ void move(char str[], int length)
 
 void solve(char input[])
 {
-    char mirrorInput[100000];
+    char mirrorInput[100] = "";
     int len = strlen(input);
     int index = 0;
     int index2 = 0;
     int index3 = 0;
     bool judge = true;
-    struct HomeEndKey HomeEndkeys[1000];
+    struct HomeEndKey HomeEndkeys[10];
+    for (int i = 0; i < 10; i++)
+    {
+        for (int d = 0; d < 100; d++)
+        {
+            HomeEndkeys[i].content[d] = '\0';
+        }
+        HomeEndkeys[i].start = 0;
+        HomeEndkeys[i].end = 0;
+    }
     for (int i = 0; i < len; i++)
     {
         if (input[i] == '[')
@@ -77,7 +90,7 @@ void solve(char input[])
         {
             continue;
         }
-        char movestr[1000];
+        char movestr[100] = "";
         strcpy(movestr, HomeEndkeys[i].content);
         int len = strlen(movestr);
         move(mirrorInput, len);
